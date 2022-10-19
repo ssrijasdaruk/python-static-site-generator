21981edbb49777764836873b62d50a4820c9d1d3
ssrijasdaruk <sirodom.srijasdaruk@accenture.com>
2022-10-19 10:17:17 +1000

    Module2 Completed

--- ssg.py
+++ ssg.py
@@ -1,8 +1,13 @@
 import typer
 from ssg.site import Site
+import ssg.parsers
 
 def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest}
+    config = {
+        "source": source,
+        "dest": dest,
+        "parsers": [ ssg.parsers.ResourceParse(), ],
+        }
 
     Site(**config).build()
 
