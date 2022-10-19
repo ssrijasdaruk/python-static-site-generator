a28ec190a832f3812997aeea9221b6fba1187842
ssrijasdaruk <sirodom.srijasdaruk@accenture.com>
2022-10-19 10:20:08 +1000

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -6,7 +6,7 @@ def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
-        "parsers": [ ssg.parsers.ResourceParse(), ],
+        "parsers": [ssg.parsers.ResourceParse()]
         }
 
     Site(**config).build()
